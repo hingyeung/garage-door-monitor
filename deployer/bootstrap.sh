6adb4031f2d0d2e5cd30eeb372984ba4848b3712
Samuel Li <samli@samuelli.net>
2018-08-07 22:50:48 +1000

    migrated from /etc/rc.local to /etc/systemd/system/garage-door-monitor.service

--- deployer/bootstrap.sh
+++ deployer/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-# run this script run /etc/rc.local
+# run this script run /etc/systemd/system/garage-door-monitor.service
 
 ENDPOINT=$1
 AWS_ROOT_CERT=$2
@@ -28,7 +28,7 @@ docker build -t ${DOCKER_IMAGE} .
 
 echo "Running Docker image ${DOCKER_IMAGE}"
 # https://bit.ly/2OiWBtR
-docker run -td --rm --cap-add SYS_RAWIO --device /dev/mem \
+docker run --rm --cap-add SYS_RAWIO --device /dev/mem \
   --name ${DOCKER_IMAGE} \
   -v ${CERTS_DIR}:/certs ${DOCKER_IMAGE} \
   -e ${ENDPOINT} \
